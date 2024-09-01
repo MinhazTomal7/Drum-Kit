acb71160ecd3464318fd0f39c5b126fde0e6525e
Minhaz Tomal <aminminhaz55@gmail.com>
2024-09-02 00:04:37 +0600

    Final code

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function playSound(key) {
+    switch (key) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default:
+            console.log(key);
+    }
+}
+
+
+document.querySelectorAll(".drum").forEach(button => {
+    button.addEventListener("click", function () {
+        playSound(this.innerHTML);
+        buttonAnimation(this.innerHTML)
+    });
+});
+
+
+document.addEventListener("keypress", function (event) {
+    playSound(event.key);
+    buttonAnimation(event.key)
+});
+
+
+function buttonAnimation(currentKey){
+    let activeButton = document.querySelector("."+currentKey)
+    activeButton.classList.add("pressed");
+    setTimeout(function (){
+        activeButton.classList.remove("pressed")
+    },100)
+}
